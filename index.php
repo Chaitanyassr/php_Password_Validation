45fc333ba86a84e7be155a5bba36c12f6920727a
Chaitanya <chaitanyas0101@gmail.com>
2020-01-22 07:43:01 +0530

    file changed

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
 	<title>password validation</title>
 </head>
 <body>
-	<form action= "<?php $_PHP_SELF ?>" method = "POST">
+	<form action= "" method = "POST">
 		<div class="FormV" class="tan">
 				<label>Enter your username</label>
-			<input for="username" placeholder="Enter your username" required>
+			<input for="username" name="name" placeholder="Enter your username" required>
 			<label>Enter your password</label>
 			<input for="password" placeholder="enter your password" required>
 			<button type="submit">submit</button>
 		</div>
+	</form>
 			  <div class="container signin" class="blue-water">
     <p>Already have an account? <a href="#">Sign in</a>.</p>
   </div>
