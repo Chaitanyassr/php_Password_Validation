d30207ebf458767cfb39c6d477af389177fb463b
Chaitanya <chaitanyas0101@gmail.com>
2020-01-22 05:51:54 +0530

    username added

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 <body>
 	<form action="">
 		<div class="">
+				<label>Enter your username</label>
+			<input for="username" placeholder="Enter your username" required>
 			<label>Enter your password</label>
 			<input for="password" placeholder="enter your password" required>
 			<button type="submit">submit</button>
