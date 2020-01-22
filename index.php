422d16138f4c69a11a513eae738a392f1e8c92a4
Chaitanya <chaitanyas0101@gmail.com>
2020-01-22 05:50:42 +0530

    added a submit button

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		<div class="">
 			<label>Enter your password</label>
 			<input for="password" placeholder="enter your password" required>
+			<button type="submit">submit</button>
 		</div>
 	</form>
 </body>
