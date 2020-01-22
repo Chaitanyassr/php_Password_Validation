9fe300be46101d205f8d561ea39b718b5d2db320
Chaitanya <chaitanyas0101@gmail.com>
2020-01-22 05:46:05 +0530

    form creation

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>password validation</title>
 </head>
 <body>
+	<form action="">
+		<div class="">
+			<label>Enter your password</label>
+			<input for="password" placeholder="enter your password" required>
+		</div>
+	</form>
 </body>
 </html>>
