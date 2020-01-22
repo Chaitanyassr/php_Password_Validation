1b4fb718743b7da38a0d4ea0d01947d29e9e0506
Chaitanya <chaitanyas0101@gmail.com>
2020-01-22 15:39:35 +0530

    password check added

--- class.php
+++ class.php
@@ -1,6 +1,7 @@
 <?php 
 class isvalidpassword{
 function check($password) {
+  $pas = "password";
   $response = "OK";
         if(strlen($password) < 8){
             $response = "Password must be at least 8 characters";
@@ -13,6 +14,8 @@ function check($password) {
 	}else if(!preg_match('@[a-z]@', $password)){
 	    $response = "Password must contain at least one number";
 	}else if(!preg_match('@[^\w]@', $password)){
+	    $response = "Password must contain at least one number";
+	}else if(!preg_match($pas, $password)){
 	    $response = "Password must contain at least one number";
 	}
 	return $response;
