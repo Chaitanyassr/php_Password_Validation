3a26a8f3a27f176038a1bded4908cf97ac98288e
Chaitanya <chaitanyas0101@gmail.com>
2020-01-22 06:50:38 +0530

    test2

--- class.php
+++ class.php
@@ -1,17 +1,6 @@
 <?php     
 echo "test1";
-class FormV
-{
-	public $LowerCase;
-	public $UpperCase;
-	public $password;
-	public $symbols;
-	
-	function  validation(argument)
-	{
-		# code...
-	}
-}
+
 
 
 
