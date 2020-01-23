8d62314b3fd6fd57be18041162d02184b75297da
Chaitanya <chaitanyas0101@gmail.com>
2020-01-23 21:16:57 +0530

    few changes

--- class.php
+++ class.php
@@ -2,21 +2,20 @@
 echo "load_test";
 class isvalidpassword{
 function check($password) {
-  $pas = "password";
   $response = "OK";
         if(strlen($password) < 8){
             $response = "Password must be at least 8 characters";
-        } else if(is_numeric($password)){
+    } else if(is_numeric($password)){
             $response = "Password must contain at least one letter";
 	} else if(!preg_match('#[0-9]#', $password)){
 	    $response = "Password must contain at least one number";
 	} else if(!preg_match('@[A-Z]@', $password)){
 	    $response = "Password must contain one capital letter";
-	}else if(!preg_match('@[a-z]@', $password)){
+	} else if(!preg_match('@[a-z]@', $password)){
 	    $response = "Password must contain at least one small letter";
-	}else if(!preg_match('@[^\w]@', $password)){
+	} else if(!preg_match('@[^\w]@', $password)){
 	    $response = "Password must contain at least one symbol";
-	}else if(!preg_match($pas, $password)){
+	} else if(!preg_match('@[^\w]@', $password)){
 	    $response = "Password cant be the word password";
 	}
 	return $response;
