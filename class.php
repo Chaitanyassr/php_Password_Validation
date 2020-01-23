5db570d5dd3df6abc77879e0b65cceced6012bda
Chaitanya <chaitanyas0101@gmail.com>
2020-01-23 21:32:03 +0530

    added preg match to match password

--- class.php
+++ class.php
@@ -5,7 +5,10 @@ function check($password) {
   $response = "OK";
         if(strlen($password) < 8){
             $response = "Password must be at least 8 characters";
-    } else if(is_numeric($password)){
+    }
+    else if(preg_match('/password/', $password)){
+	    $response = "Password cant be the word password";
+	} else if(is_numeric($password)){
             $response = "Password must contain at least one letter";
 	} else if(!preg_match('#[0-9]#', $password)){
 	    $response = "Password must contain at least one number";
@@ -15,9 +18,7 @@ function check($password) {
 	    $response = "Password must contain at least one small letter";
 	} else if(!preg_match('@[^\w]@', $password)){
 	    $response = "Password must contain at least one symbol";
-	} else if(preg_match("/Password/", $password)){
-	    $response = "Password cant be the word password";
-	}
+	} 
 	return $response;
 }
 }
